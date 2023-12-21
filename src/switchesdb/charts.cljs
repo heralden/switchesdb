c82eab0af1af863a2df486e462e5f9e3ef64d699
heralden <regen@anche.no>
2023-12-21 12:10:15 +0100

    Use full URL in vegalite to fix "open in vega editor"

--- src/switchesdb/charts.cljs
+++ src/switchesdb/charts.cljs
@@ -17,7 +17,7 @@
   ["#1f77b4" "#aec7e8" "#ff7f0e" "#ffbb78" "#2ca02c" "#98df8a" "#d62728" "#ff9896" "#9467bd" "#c5b0d5" "#8c564b" "#c49c94" "#e377c2" "#f7b6d2" "#7f7f7f" "#c7c7c7" "#bcbd22" "#dbdb8d" "#17becf" "#9edae5"])
 
 (defn layer-spec [{:keys [csv-file source display-name switch-name]}]
-  {:data {:url (str "data/" csv-file)}
+  {:data {:url (str js/window.location.origin "/data/" csv-file)}
    :transform
    [{:filter {:field "displacement"
               :range [0 nil]}}
